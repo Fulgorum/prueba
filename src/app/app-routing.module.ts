9e0e01fcc49b3b18ea2e61026ec110ba01f64b08
fulgorum <eduardo.caballerpons@cieep.com>
2023-12-27 13:51:39 +0100

    Actualizado...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./pages/chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
 ];
 
 @NgModule({
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {UsuarioService} from "../../services/usuario.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,18 +9,64 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterPage implements OnInit {
   intercambiarCards: boolean = true;
+  nombre: string = '';
+  pass: string = '';
+  repeatPass: string = '';
+  public mensajeErrorNombre: string = '';
+  public mensajeErrorPass: string = '';
+  public mensajeErrorEquals: string = '';
 
 
-  constructor() {
+  constructor(private dataService: UsuarioService, private router: Router) {
   }
+
 
   ngOnInit() {
   }
 
 
-
   intercambiarPosition() {
     this.intercambiarCards = !this.intercambiarCards;
+    this.reset();
+  }
+
+  reset() {
+    this.mensajeErrorNombre = '';
+    this.mensajeErrorPass = '';
+    this.mensajeErrorEquals = '';
+    this.nombre = '';
+    this.pass = '';
+    this.repeatPass = '';
+  }
+
+  validarNombre() {
+    this.mensajeErrorNombre = this.nombre.length < 3 ? 'El nombre debe tener al menos 3 caracteres.' : '';
+  }
+
+  validarPass() {
+    this.mensajeErrorPass = this.pass.length < 8 ? 'La contraseña debe tener al menos 8 caracteres.' : '';
+  }
+
+  validarIgualdadPass() {
+    if (this.repeatPass != this.pass) {
+      this.mensajeErrorEquals = 'Las contraseñas no coinciden';
+    } else {
+      this.mensajeErrorEquals = '';
+    }
+  }
+
+  guardarNombre() {
+    if (this.nombre.length < 3 || this.pass.length < 8) {
+      this.validarNombre()
+      this.validarPass()
+      this.validarIgualdadPass()
+    } else {
+      this.dataService.setNombre(this.nombre);
+      this.router.navigate(['/tabs/home']);
+    }
+
   }
 
 }
+
+
